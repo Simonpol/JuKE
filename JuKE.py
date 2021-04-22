8b6db24e1573d0e99d9147b61880c0b5208c2985
Lupanov <CSTLUPAND@CORP.JTI.COM>
2021-04-22 16:51:25 +0300

    Finally Scelet version

--- JuKE.py
+++ JuKE.py
@@ -10,8 +10,6 @@ def generalContent():
     general_body = LabelFrame(window, text ="Basical",)
     general_body.pack(expand = True, fill = BOTH, padx=5, pady=5)
 
-    # if (adOperationsContent):
-    #     general_body.pack_forget()
 
 def adOperationsContent():
     global adOperations_body
@@ -22,6 +20,7 @@ def adOperationsContent():
     
         
 # Menu bar with buttons
+# Action after clicking "General Buttons"
 
 def generalbtn_click_button():
     global clicks
@@ -34,7 +33,7 @@ def generalbtn_click_button():
         adOperationsBtn.config(state = "normal")
 
      
-
+# Actiond after "AdOperations" click button
 def adoperationsbtn_click_button():
     clicks = 0
     clicks +=1
@@ -43,20 +42,12 @@ def adoperationsbtn_click_button():
         general_body.pack_forget()
         adOperationsBtn.config(state = "disabled")
         generalBtn.config(state = "normal")
-
-
+# Menu bar
 menubar = Frame(window, bg = 'red', width = 600)
-generalBtn = Button(menubar, relief = RAISED, text='General', command = generalbtn_click_button)
+generalBtn = Button(menubar, relief = RAISED, text='General', command = generalbtn_click_button, state = DISABLED)
 adOperationsBtn = Button(menubar, relief = RAISED, text = 'ADOperarions', command = adoperationsbtn_click_button)
 menubar.pack(side = TOP, anchor = NW)
 generalBtn.pack(side = LEFT)
 adOperationsBtn.pack()
-
-# Кнопка поиска и событие при нажатии
-# explorerBtn = Button(window, text="Open")
-# explorerBtn.grid(column=1, row=0)
-# def explorerBtnAct(event):
-#     dirlist = filedialog.askdirectory()
-# explorerBtn.bind("<Button-1>", explorerBtnAct)
-
+generalContent()
 window.mainloop()
