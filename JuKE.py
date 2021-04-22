0c5fb50abb766a99090d1b2e3f6620f485718773
Lupanov <CSTLUPAND@CORP.JTI.COM>
2021-04-22 16:02:17 +0300

    Compleated Skelet developement v1.0

--- JuKE.py
+++ JuKE.py
@@ -6,20 +6,48 @@ window.geometry('600x400+200+100')
 window.resizable(0,0)
 window.title("JuKE")
 def generalContent():
+    global general_body
     general_body = LabelFrame(window, text ="Basical",)
     general_body.pack(expand = True, fill = BOTH, padx=5, pady=5)
 
+    # if (adOperationsContent):
+    #     general_body.pack_forget()
+
 def adOperationsContent():
+    global adOperations_body
     adOperations_body = LabelFrame(text="ADOperations")
     adOperations_body.pack(expand = True, fill = BOTH, padx=5, pady=5)
 
-def isbuttonmenuPressed():
-    print("hello")
+
+    
         
 # Menu bar with buttons
+
+def generalbtn_click_button():
+    global clicks
+    clicks = 0
+    clicks += 1
+    if (clicks == 1):
+        generalContent()
+        adOperations_body.pack_forget()
+        generalBtn.config(state = "disabled")
+        adOperationsBtn.config(state = "normal")
+
+     
+
+def adoperationsbtn_click_button():
+    clicks = 0
+    clicks +=1
+    if (clicks == 1):
+        adOperationsContent()
+        general_body.pack_forget()
+        adOperationsBtn.config(state = "disabled")
+        generalBtn.config(state = "normal")
+
+
 menubar = Frame(window, bg = 'red', width = 600)
-generalBtn = Button(menubar, relief = RAISED, text='General', command = generalContent)
-adOperationsBtn = Button(menubar, relief = RAISED, text = 'ADOperarions')
+generalBtn = Button(menubar, relief = RAISED, text='General', command = generalbtn_click_button)
+adOperationsBtn = Button(menubar, relief = RAISED, text = 'ADOperarions', command = adoperationsbtn_click_button)
 menubar.pack(side = TOP, anchor = NW)
 generalBtn.pack(side = LEFT)
 adOperationsBtn.pack()
