b01f0b64c60ae169b97c03eaba35a30aad353d0b
Lupanov <CSTLUPAND@CORP.JTI.COM>
2021-04-23 21:12:49 +0300

    SyslemInfo block v0.1

--- JuKE.py
+++ JuKE.py
@@ -1,6 +1,10 @@
 import sys
+import os
 from tkinter import *
-# from tkinter import filedialog
+
+computername = os.environ['COMPUTERNAME']
+username = os.environ['USERDOMAIN'] + '\\' + os.environ['USERNAME']
+
 window = Tk()
 window.geometry('600x400+200+100')
 window.resizable(0,0)
@@ -9,7 +13,35 @@ def generalContent():
     global general_body
     general_body = LabelFrame(window, text ="Basical",)
     general_body.pack(expand = True, fill = BOTH, padx=5, pady=5)
+    # System information field
+    sysinfo_body = LabelFrame(general_body, text = "System Info", width = 200, height = 400)
+    # Computer Name block
 
+    com_infoblock = Frame( sysinfo_body, width =100, height = 10)
+    com_infolabel = Label(com_infoblock, text = "Computer Name")
+    com_computername = Entry(com_infoblock, justify = CENTER, fg = 'blue')
+    com_computername.insert(0,computername)
+    # User Name block
+    use_infoblock = Frame( sysinfo_body, width=100, height=10)
+    use_infolabel = Label( use_infoblock, text="User Name           ")
+    use_username = Entry(use_infoblock, justify = CENTER, fg = 'blue')
+    use_username.insert(0, username)
+    # Computer Name Block displaying
+    com_infolabel.pack(side = LEFT)
+    com_computername.pack(side = RIGHT)
+    com_infoblock.pack(padx =5, pady=5)
+    # User Name block displaying
+    use_infolabel.pack(side = LEFT)
+    use_username.pack(side = RIGHT)
+    use_infoblock.pack(padx=5, pady=5)
+
+
+    sysinfo_body.pack(padx=5, pady= 5, side = RIGHT, anchor = NE)
+    
+
+    
+    
+    
 
 def adOperationsContent():
     global adOperations_body
@@ -43,7 +75,7 @@ def adoperationsbtn_click_button():
         adOperationsBtn.config(state = "disabled")
         generalBtn.config(state = "normal")
 # Menu bar
-menubar = Frame(window, bg = 'red', width = 600)
+menubar = Frame(window, width = 600)
 generalBtn = Button(menubar, relief = RAISED, text='General', command = generalbtn_click_button, state = DISABLED)
 adOperationsBtn = Button(menubar, relief = RAISED, text = 'ADOperarions', command = adoperationsbtn_click_button)
 menubar.pack(side = TOP, anchor = NW)
